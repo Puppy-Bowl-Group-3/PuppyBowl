2257adb9a0b8a170d8a9636b6bca0adf7250a134
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-06-01 19:21:48 -0700

    updated correct files

--- script.js
+++ script.js
@@ -1,159 +1,96 @@
-const newPartyForm = document.querySelector('#new-party-form');
-const partyContainer = document.querySelector('#party-container');
+const playerContainer = document.getElementById('all-players-container');
+const newPlayerFormContainer = document.getElementById('new-player-form');
 
-const PARTIES_API_URL =
-  'http://fsa-async-await.herokuapp.com/api/workshop/parties';
-const GUESTS_API_URL =
-  'http://fsa-async-await.herokuapp.com/api/workshop/guests';
-const RSVPS_API_URL = 'http://fsa-async-await.herokuapp.com/api/workshop/rsvps';
-const GIFTS_API_URL = 'http://fsa-async-await.herokuapp.com/api/workshop/gifts';
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = 'YOUR COHORT NAME HERE';
+// Use the APIURL variable for fetch requests
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
-// get all parties
-const getAllParties = async () => {
-  try {
-    const response = await fetch(PARTIES_API_URL);
-    const parties = await response.json();
-    return parties;
-  } catch (error) {
-    console.error(error);
-  }
+/**
+ * It fetches all players from the API and returns them
+ * @returns An array of objects.
+ */
+const fetchAllPlayers = async () => {
+    try {
+
+    } catch (err) {
+        console.error('Uh oh, trouble fetching players!', err);
+    }
 };
 
-// get single party by id
-const getPartyById = async (id) => {
-  try {
-    const response = await fetch(`${PARTIES_API_URL}/${id}`);
-    const party = await response.json();
-    // console.log(party);
-    return party;
-  } catch (error) {
-    console.error(error);
-  }
+const fetchSinglePlayer = async (playerId) => {
+    try {
+
+    } catch (err) {
+        console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+    }
 };
 
-// delete party
-const deleteParty = async (id) => {
-  // your code here
-  try {
-    const response = await fetch(`${PARTIES_API_URL}/${id}`, {
-      method: "DELETE",
-    });
-    const party = await response.json();
-    getAllParties();
-    window.location.reload();
-  } catch (error) {
-      console.log(error);
-  }
+const addNewPlayer = async (playerObj) => {
+    try {
+
+    } catch (err) {
+        console.error('Oops, something went wrong with adding that player!', err);
+    }
 };
 
-// render a single party by id
-const renderSinglePartyById = async (id) => {
-  try {
-    // fetch party details from server
-    const party = await getPartyById(id);
-    // console.log(party.name);
-    // GET - /api/workshop/guests/party/:partyId - get guests by party id
-    const guestsResponse = await fetch(`${GUESTS_API_URL}/party/${id}`);
-    const guests = await guestsResponse.json();
+const removePlayer = async (playerId) => {
+    try {
 
-    // GET - /api/workshop/rsvps/party/:partyId - get RSVPs by partyId
-    const rsvpsResponse = await fetch(`${RSVPS_API_URL}/party/${id}`);
-    const rsvps = await rsvpsResponse.json();
-
-    // GET - get all gifts by party id - /api/workshop/parties/gifts/:partyId -BUGGY?
-    // const giftsResponse = await fetch(`${PARTIES_API_URL}/party/gifts/${id}`);
-    // const gifts = await giftsResponse.json();
-
-    // create new HTML element to display party details
-    let partyHTML = `
-            <h2>${party.name}</h2>
-            <p>${party.date}</p>
-            <p>${party.time}</p>
-            <p>${party.location}</p>
-            <p>${party.description}</p>
-            <h3>Guests:</h3>
-            <ul>
-            ${guests
-              .map(
-                (guest, index) => `
-              <li>
-                <div>${guest.name}</div>
-                <div>${rsvps[index].status}</div>
-              </li>
-            `
-              )
-              .join('')}
-          </ul>
-          
-
-
-            <button class="close-button">Close</button>
-        `;
-    partyContainer.innerHTML = partyHTML;
-    
-
-    // add event listener to close button
-    const closeButton = partyContainer.querySelector('.close-button');
-    closeButton.addEventListener('click', async () => {
-      // partyDetailsElement.remove();
-      const party = await getAllParties();
-      renderParties(party);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+    } catch (err) {
+        console.error(
+            `Whoops, trouble removing player #${playerId} from the roster!`,
+            err
+        );
+    }
 };
 
-// render all parties
-const renderParties = async (parties) => {
-  try {
-    partyContainer.innerHTML = '';
-    parties.forEach((party) => {
-      // console.log(party.id);
-      const partyElement = document.createElement('div');
-      partyElement.classList.add('party');
-      partyElement.innerHTML = `
-                <h2>${party.name}</h2>
-                <p>${party.description}</p>
-                <p>${party.date}</p>
-                <p>${party.time}</p>
-                <p>${party.location}</p>
-                <button class="details-button" data-id="${party.id}">See Details</button>
-                <button class="delete-button" data-id="${party.id}">Delete</button>
-            `;
-      partyContainer.appendChild(partyElement);
-
-      // see details
-      const detailsButton = partyElement.querySelector('.details-button');
-      detailsButton.addEventListener('click', (event) => {
-        // your code here
-        event.preventDefault();
-        renderSinglePartyById(party.id);
-        // console.log(party.id);
-      });
-
-      // delete party
-      const deleteButton = partyElement.querySelector('.delete-button');
-      deleteButton.addEventListener('click', async (event) => {
-        // your code here
-        event.preventDefault();
-        deleteParty(party.id);
+/**
+ * It takes an array of player objects, loops through them, and creates a string of HTML for each
+ * player, then adds that string to a larger string of HTML that represents all the players. 
+ * 
+ * Then it takes that larger string of HTML and adds it to the DOM. 
+ * 
+ * It also adds event listeners to the buttons in each player card. 
+ * 
+ * The event listeners are for the "See details" and "Remove from roster" buttons. 
+ * 
+ * The "See details" button calls the `fetchSinglePlayer` function, which makes a fetch request to the
+ * API to get the details for a single player. 
+ * 
+ * The "Remove from roster" button calls the `removePlayer` function, which makes a fetch request to
+ * the API to remove a player from the roster. 
+ * 
+ * The `fetchSinglePlayer` and `removePlayer` functions are defined in the
+ * @param playerList - an array of player objects
+ * @returns the playerContainerHTML variable.
+ */
+const renderAllPlayers = (playerList) => {
+    try {
         
-      });
-    });
-  } catch (error) {
-    console.error(error);
-  }
+    } catch (err) {
+        console.error('Uh oh, trouble rendering players!', err);
+    }
 };
 
-// init function
+
+/**
+ * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
+ * fetches all players from the database, and renders them to the DOM.
+ */
+const renderNewPlayerForm = () => {
+    try {
+        
+    } catch (err) {
+        console.error('Uh oh, trouble rendering the new player form!', err);
+    }
+}
+
 const init = async () => {
-  // your code here
-  const parties = await getAllParties();
-  const partyId = await getPartyById();
+    const players = await fetchAllPlayers();
+    renderAllPlayers(players);
 
-  renderParties(parties);
-  // renderSinglePartyById(partyId);
-};
+    renderNewPlayerForm();
+}
 
 init();

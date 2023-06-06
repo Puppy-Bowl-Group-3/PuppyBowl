c20b037c58bac08c469e8f3112a4f5911eea7ad6
swoops503 <james.roundy4@gmail.com>
2023-06-05 18:39:33 -0700

    first commit, added fetchAllPlayers & fetchSinglePlayer functions

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2302-ACC-PT-WEB-PT-D';
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
 
 /**
  * It fetches all players from the API and returns them
  * @returns An array of objects.
  */
 const fetchAllPlayers = async () => {
+    console.log('this workin?')
     try {
-
+    const response = await fetch(APIURL);
+    const allPlayers = await response.json();
+    console.log(allPlayers);
+    return allPlayers;
+    
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
@@ -20,7 +25,9 @@ const fetchAllPlayers = async () => {
 
 const fetchSinglePlayer = async (playerId) => {
     try {
-
+    const response = await fetch(`${APIURL}/${id}`);
+    const singlePlayer = response.json;
+    return singlePlayer;
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
